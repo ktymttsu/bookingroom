1eaead31c32304d45c0d219833ac4eb45aaea29d
ktymttsuy <Ka0d5C0pe>
2013-01-30 20:33:47 +0900

    テーブル構成調整
    予約リストと部屋リストのテーブルを追加

--- xoops_trust_path/modules/roombooking/sql/mysql.sql
+++ xoops_trust_path/modules/roombooking/sql/mysql.sql
@@ -1,6 +1,23 @@
-CREATE TABLE `{prefix}_{dirname}_room_list` (
-  `room_list_id` int(11) unsigned NOT NULL  auto_increment,
+CREATE TABLE `{prefix}_{dirname}_booking` (
+  `booking_id` int(11) unsigned NOT NULL  auto_increment,
+  `booking_room_id` int(11) unsigned NOT NULL,
+  `booking_user_id` int(11) unsigned NOT NULL,
+  `booking_num` int(11),
+  `booking_comment` text,
   `title` varchar(255) NOT NULL,
   `posttime` int(11) unsigned NOT NULL,
-  PRIMARY KEY  (`room_list_id`)) ENGINE=MyISAM;
+  PRIMARY KEY  (`booking_id`)) ENGINE=MyISAM;
+
+CREATE TABLE `{prefix}_{dirname}_room` (
+  `room_id` int(11) unsigned NOT NULL  auto_increment,
+  `room_num` int(11),
+  `room_type` boolean,
+  `room_speciality1` boolean,
+  `room_speciality2` boolean,
+  `room_speciality3` boolean,
+  `room_comment` text,
+  `room_owner_list` text,
+  `title` varchar(255) NOT NULL,
+  `posttime` int(11) unsigned NOT NULL,
+  PRIMARY KEY  (`room_id`)) ENGINE=MyISAM;
 
